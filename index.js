2f98a75f1a3eb5cbdb8b5652e7ffacb7bd5d3bcf
Clayton <myCCBarnard@gmail.com>
2018-07-26 14:24:03 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,48 @@
+//Static Ascii
+import coffee from require('./ascii/coffie.ascii');
+export const coffee = coffee;
+
+import decoration from require('./ascii/decoration.ascii')
+export const decoration = decoration;
+
+import finger from require('./ascii/finger.ascii')
+export const finger = finger;
+
+import gimme from require('./ascii/gimme.ascii')
+export const gimme = gimme;
+
+import hold_up from require('./ascii/hold_up.ascii')
+export const hold_up = hold_up;
+
+import last_supper from require('./ascii/last_supper.ascii')
+export const last_supper = last_supper;
+
+import loveyou from require('./ascii/loveyou.ascii')
+export const loveyou = loveyou;
+
+import nuke from require('./ascii/nuke.ascii')
+export const nuke = nuke;
+
+import oops from require('./ascii/oops.ascii')
+export const oops = oops;
+
+import shrug from require('./ascii/shrug.ascii')
+export const shrug = shrug;
+
+import sleeping from require('./ascii/sleeping.ascii')
+export const sleeping = sleeping;
+
+import table_flip from require('./ascii/table_flip.ascii')
+export const table_flip = table_flip;
+
+import troll from require('./ascii/troll.ascii')
+export const troll = troll;
+
+import wave from require('./ascii/wave.ascii')
+export const wave = wave;
+
+import zoidberg from require('./ascii/zoidberg.ascii')
+export const zoidberg = zoidberg;
+
+//Ascii Helpers
+export * from './src/decorate';
---
+++ src/decorate.js
@@ -0,0 +1,6 @@
+
+
+export default function decorate (s) {
+    const start = '▂▃▅▇█▓▒░۩۞۩    '; const end = '    ۩۞۩░▒▓█▇▅▃▂';
+    return start + s + end;
+}
